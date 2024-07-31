8f9867a16f1311414140b49e08fe77948f0a52c8
Marcos G. Zimmermann <mgzmaster@gmail.com>
2024-07-31 17:52:10 -0300

    feat: remove group, order limit and offset from count

--- lib/esse/active_record/collection.rb
+++ lib/esse/active_record/collection.rb
@@ -90,7 +90,7 @@ module Esse
       end
 
       def count
-        dataset.except(:includes, :preload, :eager_load).count
+        dataset.except(:includes, :preload, :eager_load, :order, :limit, :offset).count
       end
       alias_method :size, :count
 
